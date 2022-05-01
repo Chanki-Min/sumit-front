dedf6a2b185477a95cb7258bf0ece694fb49e247
Chanki Min <alscksrl2010@gmail.com>
2022-05-01 16:35:19 +0900

    Fix mockup data by change of type def

--- src/api/fetchBlock.ts
+++ src/api/fetchBlock.ts
@@ -1,42 +1,42 @@
 import { Block } from "../models/block";
 
- const fetchBlock = async() => {
-    return todo_slide;
-}
+const fetchBlock = async () => {
+  return todo_slide;
+};
 
 const todo_slide: Block = {
-    uuid: 1,
-    type: "to_do_list",
-    properties: { text: "부모 블록", checked: false },
-    parent: null,
-    order: 0,
-    children: [
-      {
-        uuid: 2,
-        type: "plain_text",
-        properties: { text: "텍스트 1" },
-        parent: 1,
-        children: [
-          {
-            uuid: 3,
-            type: "plain_text",
-            properties: { text: "텍스트 2" },
-            parent: 1,
-            children: [],
-            order: 1
-          },
-        ],
-        order: 0
-      },
-      {
-        uuid: 3,
-        type: "plain_text",
-        properties: { text: "텍스트 2" },
-        parent: 1,
-        children: [],
-        order: 1
-      },
-    ],
-  };
+  uuid: "1",
+  type: "to_do_list",
+  properties: { text: "부모 블록", checked: false },
+  parent: null,
+  order: 0,
+  children: [
+    {
+      uuid: "2",
+      type: "plain_text",
+      properties: { text: "텍스트 1" },
+      parent: "1",
+      children: [
+        {
+          uuid: "3",
+          type: "plain_text",
+          properties: { text: "텍스트 2" },
+          parent: "1",
+          children: [],
+          order: 1,
+        },
+      ],
+      order: 0,
+    },
+    {
+      uuid: "4",
+      type: "plain_text",
+      properties: { text: "텍스트 2" },
+      parent: "1",
+      children: [],
+      order: 1,
+    },
+  ],
+};
 
 export default fetchBlock;
