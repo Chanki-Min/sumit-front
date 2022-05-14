cddf626b3905e7bc485259ba22bfb458ae637c8a
Chanki Min <alscksrl2010@gmail.com>
2022-05-15 00:43:32 +0900

    Add deps (font, lodash), modify next.config for lodash static tree shaking

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    modularizeImports: {
+      lodash: {
+        transform: "lodash/{{member}}",
+      },
+    },
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true, // enable swc complier's styled-component support
     swcMinify: true, // enable swc minifier, https://nextjs.org/docs/advanced-features/compiler#minification
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
-  }
+    domains: ["lh3.googleusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
