c828fda43dd4afc3f4bdb4479a8c9696e7b66b63
Chanki Min <alscksrl2010@gmail.com>
2022-05-28 13:52:09 +0900

    Export more constants

--- src/Contstants.ts
+++ src/Contstants.ts
@@ -1,1 +1,5 @@
 export const PLACEHOLDER = '명령어 사용시 "/"를 입력하세요' as const;
+
+export const IS_SERVER_SIDE = typeof window === "undefined";
+
+export const IS_DEV_RUNTIME = process.env.NODE_ENV === "development";
