f5435622dd9af6c8573872d091d4cf801000a764
Dayeon Lee <dayoni99@gmail.com>
2022-05-30 18:42:27 +0900

    feature(대시보드) : projectPreview UI 개선  (#9)
    * Fix css about projectContainer
    
    * Fix UI

--- src/components/commons/projectPreview.tsx
+++ src/components/commons/projectPreview.tsx
@@ -1,9 +1,9 @@
-import { OmittedPage } from "../../models/page";
+import { OmittedPage } from '../../models/page';
 
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image } from 'semantic-ui-react';
 
-import styled from "styled-components";
-import Link from "next/link";
+import styled from 'styled-components';
+import Link from 'next/link';
 
 /**
  * /dashboard 페이지
@@ -12,71 +12,88 @@ import Link from "next/link";
  */
 
 interface ProjectPreviewProps {
-  project: OmittedPage;
+	project: OmittedPage;
 }
 
 const ProjectPreview: React.FC<ProjectPreviewProps> = ({ project }) => {
-  return (
-    <Link
-      href={{
-        pathname: `/edit/${project.uuid}`,
-      }}
-      passHref
-    >
-      <ProjectContainer>
-        <div className="project_img">
-          <Image
-            fluid
-            label={{
-              color: project.share ? "blue" : "red",
-              corner: "right",
-              icon: project.share ? "globe" : "heart",
-            }}
-            src="/img/mountain.png"
-            alt="project preview image"
-          />
-        </div>
-        <div className="project_header">
-          {project.title}
-          <i className="ellipsis horizontal icon" />
-        </div>
-        <div className=""></div>
-        <Card.Content>
-          <Card.Description>{project.description}</Card.Description>
-          {project.hashtags.map((h) => (
-            <HashTag key={h}>{h}</HashTag>
-          ))}
-        </Card.Content>
-      </ProjectContainer>
-    </Link>
-  );
+	return (
+		<Link
+			href={{
+				pathname: `/edit/${project.uuid}`,
+			}}
+			passHref
+		>
+			<ProjectContainer>
+				<div className='project_img'>
+					<Image
+						fluid
+						label={{
+							color: project.share ? 'blue' : 'red',
+							corner: 'right',
+							icon: project.share ? 'globe' : 'heart',
+						}}
+						src='/img/projectPreviewImg.png'
+						alt='project preview image'
+					/>
+				</div>
+				<div className='project_header'>
+					{project.title}
+					<button className='menu_btn'>
+						<i className='ellipsis horizontal icon' />
+					</button>
+				</div>
+				<div className='project_description'>{project.description}</div>
+				<div className='project_hashtags'>
+					{project.hashtags.map((h) => (
+						<HashTag key={h}>{h}</HashTag>
+					))}
+				</div>
+			</ProjectContainer>
+		</Link>
+	);
 };
 
 const ProjectContainer = styled.div`
-  width: 500px;
-  height: 350px;
-  margin: 5% 0 0 6%;
-  // border: 1px solid black;
+	width: 500px;
+	height: 350px;
+	margin: 5% 0 0 6%;
+	// border: 1px solid black;
 
-  & > .project_img {
-    height: 220px;
-    overflow: hidden;
-    border: 1px solid black;
-  }
+	& > .project_img {
+		height: 65%;
+		overflow: hidden;
+		border: 2px solid gray;
+	}
 
-  & > .project_header {
-    margin-top: 2%;
-    font-size: 20px;
-    font-weight: 700;
-  }
+	& > .project_header {
+		margin-top: 2%;
+		font-size: 20px;
+		font-weight: 700;
+		// border: 1px solid black;
+
+		& > .menu_btn {
+			border: none;
+			float: right;
+			background-color: transparent;
+		}
+	}
+
+	& > .project_description {
+		margin: 3% 0 3% 0;
+		font-size: 15px;
+	}
+
+	& > .project_hashtags {
+		// border: 1px solid black;
+	}
 `;
 
 const HashTag = styled.button`
-  height: 25px;
-  margin: 1%;
-  border-radius: 30px;
-  background-color: lightblue;
-  border: none;
+	height: 25px;
+	margin: 0 1% 0 0;
+	border-radius: 30px;
+	background-color: lightblue;
+	border: none;
 `;
 
 export default ProjectPreview;
