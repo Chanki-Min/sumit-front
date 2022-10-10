af53ebfe5261ff5918f27c723fd907823bbd3acc
icethief <icethief@banksalad.com>
2022-10-10 16:23:02 +0900

    페이지 생성 추가

--- src/api/page/fetchPage.ts
+++ src/api/page/fetchPage.ts
@@ -3,66 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 import { IS_DEV_RUNTIME, IS_SERVER_SIDE } from "../../Contstants";
 import { QueryClient, useQuery } from "react-query";
 import { pageMockData } from "./pageDataMock";
+import axios from "axios";
 
-export function fetchPages<B extends boolean>(
-  withSlides: B
-): B extends true ? Promise<Page[]> : Promise<Omit<Page, "slides">[]>;
-
-export async function fetchPages(withSlides: boolean) {
+export async function fetchPages(token: string) {
   if (IS_DEV_RUNTIME) {
-    console.log("API:fetchPages, args:", withSlides);
+    console.log("API:fetchPages, args:");
   }
 
-  if (withSlides === false) {
-    return pageMockData;
-  }
-
-  return pageMockData.map(omitSlide);
+  return (await axios.get<Page[]>(`/api/pages`)).data;
 }
-
-async function fetchPageById(pageId: string) {
-  if (IS_DEV_RUNTIME) {
-    console.log("API:fetchPageById, args:", pageId);
-  }
-
-  const dataOrUndefined = pageMockData.find((p) => p.uuid === pageId);
-  if (typeof dataOrUndefined === "undefined") {
-    throw new Error(`pageId: ${pageId} not found`);
-  }
-
-  return dataOrUndefined;
-}
-
-const omitSlide = (md: Page): Omit<Page, "slides"> => {
-  const { slides, ...rest } = md;
-  return rest;
-};
-
-export const usePagesQuery = (withSlides: boolean) => {
-  return useQuery<Page[] | Omit<Page, "slides">[]>(
-    ["page", withSlides],
-    () => {
-      if (withSlides) {
-        return fetchPages(withSlides) as Promise<Page[]>;
-      }
-
-      return fetchPages(withSlides);
-    },
-    {
-      refetchOnWindowFocus: false,
-    }
-  );
-};
-
-export const prefetchPagesQuerySsr = (
-  queryClient: QueryClient,
-  withSlides: boolean
-) => {
-  if (!IS_SERVER_SIDE) {
-    throw new Error("preFetching should be called within server-side only");
-  }
-
-  return queryClient.prefetchQuery(["page", withSlides], () => {
-    return fetchPages(withSlides);
-  });
-};
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,23 @@
 import {
-	getSession,
-	UserProfile,
-	withPageAuthRequired,
-} from '@auth0/nextjs-auth0';
+  getAccessToken,
+  getSession,
+  UserProfile,
+  withPageAuthRequired,
+} from "@auth0/nextjs-auth0";
 
-import { OmittedPage, transformPageToOmittedPage } from '../models/page';
-import ProjectPreview from '../components/commons/projectPreview';
+import { OmittedPage, transformPageToOmittedPage } from "../models/page";
+import ProjectPreview from "../components/commons/projectPreview";
 
-import Link from 'next/link';
-import styled from 'styled-components';
-import { Grid } from 'semantic-ui-react';
-import { fetchPages } from '../api/page/fetchPage';
+import Link from "next/link";
+import styled from "styled-components";
+import { Grid } from "semantic-ui-react";
+import { fetchPages } from "../api/page/fetchPage";
+import axios from "axios";
 
 interface DashboardProps {
-	user?: UserProfile;
-	// pages: Omit<Page, 'slides'>[];
-	pages: OmittedPage[];
+  user?: UserProfile;
+  // pages: Omit<Page, 'slides'>[];
+  pages: OmittedPage[];
 }
 /**
  * /dashboard 페이지
@@ -23,69 +25,83 @@ interface DashboardProps {
  * @returns NextPage
  */
 const Dashboard = ({ user, pages }: DashboardProps) => {
-	if (!Array.isArray(pages) || !user) {
-		return <></>;
-	}
+  if (!Array.isArray(pages) || !user) {
+    return <></>;
+  }
 
-	//   console.log(pages, user);
+  const createPage = async () => {
+    const createResponse = await axios.post("/api/pages", {
+      title: "새 페이지",
+    });
 
-	return (
-		<Grid rows='3' columns={2}>
-			{pages.map((page) => (
-				<ProjectPreview key={page.uuid} project={page} />
-			))}
-			<AddNewProject>
-				<Link href={'/edit/test_page'} passHref>
-					<button className='add_btn'>
-						<i className='plus icon' />
-					</button>
-				</Link>
-				<div className='add_title'>새로운 페이지 시작하기</div>
-			</AddNewProject>
-		</Grid>
-	);
+    window.location.reload();
+  };
+
+  return (
+    <Grid rows="3" columns={2}>
+      {pages.map((page) => (
+        <ProjectPreview key={page.uuid} project={page} />
+      ))}
+      <AddNewProject>
+        <button className="add_btn" onClick={createPage}>
+          <i className="plus icon" />
+        </button>
+
+        <div className="add_title">새로운 페이지 시작하기</div>
+      </AddNewProject>
+    </Grid>
+  );
 };
 
 export const getServerSideProps = withPageAuthRequired<DashboardProps>({
-	async getServerSideProps(context) {
-		// Getting user data from Auth0
-		const user = getSession(context.req, context.res)?.user;
-		const pages = await fetchPages(false);
+  async getServerSideProps(context) {
+    // Getting user data from Auth0
+    const user = getSession(context.req, context.res)?.user;
+    const accessToken = await (
+      await getAccessToken(context.req, context.res)
+    ).accessToken;
 
-		// Pass user and page data to render method
-		return {
-			props: {
-				user: user,
-				pages: pages.map(transformPageToOmittedPage),
-			},
-		};
-	},
+    const apiPort = process.env.API_PORT || 8000;
+    const response = await axios.get(`http://localhost:${apiPort}/pages`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    // Pass user and page data to render method
+    return {
+      props: {
+        user: user,
+        pages: response.data.map(transformPageToOmittedPage),
+      },
+    };
+  },
 });
 
 export default Dashboard;
 
 const AddNewProject = styled.div`
-	width: 500px;
-	height: 350px;
-	margin: 5% 0 0 6%;
-	// border: 1px solid black;
+  width: 500px;
+  height: 350px;
+  margin: 5% 0 0 6%;
+  // border: 1px solid black;
 
-	& > .add_btn {
-		width: 100%;
-		height: 65%;
-		border: 2px solid gray;
+  & > .add_btn {
+    width: 100%;
+    height: 65%;
+    border: 2px solid gray;
 
-		&:hover {
-			color: teal;
-			border: 2px solid teal;
-			background-color: lightblue;
-		}
-	}
+    &:hover {
+      color: teal;
+      border: 2px solid teal;
+      background-color: lightblue;
+    }
+  }
 
-	& > .add_title {
-		margin-top: 4%;
-		font-size: 20px;
-		font-weight: 700;
-		text-align: center;
-	}
+  & > .add_title {
+    margin-top: 4%;
+    font-size: 20px;
+    font-weight: 700;
+    text-align: center;
+  }
 `;
