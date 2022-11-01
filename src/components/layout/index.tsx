a729aa46c1db24b0b60fb460ed679fd480a95d70
dayonxxi <dayoni99@gmail.com>
2022-11-01 21:39:08 +0900

    Enable NEXT_PUBLIC_API_MOCKIG

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,27 +1,27 @@
-import { useUser } from "@auth0/nextjs-auth0";
-import Image from "next/image";
-import Link from "next/link";
-import { PropsWithChildren, useEffect, useState } from "react";
-import styled from "styled-components";
-import Header from "./Header";
+import { useUser } from '@auth0/nextjs-auth0';
+import Image from 'next/image';
+import Link from 'next/link';
+import { PropsWithChildren, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Header from './Header';
 
 const Layout: React.FC<PropsWithChildren> = (props) => {
-  const { user } = useUser();
+	const { user } = useUser();
 
-  return (
-    <>
-      <Header />
-      <LayoutContent>{props.children} </LayoutContent>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<LayoutContent>{props.children} </LayoutContent>
+		</>
+	);
 };
 
 export default Layout;
 
 const LayoutContent = styled.section`
-  position: relative;
-  width: 100%;
-  /* margin-top: 60px; */
-  padding: 0.05px; // to disable margin collapsing
-  height: calc(100% - 70px);
+	position: relative;
+	width: 100%;
+	/* margin-top: 60px; */
+	padding: 0.05px; // to disable margin collapsing
+	height: calc(100% - 70px);
 `;
--- src/components/main/LoginSection.tsx
+++ src/components/main/LoginSection.tsx
@@ -1,73 +1,72 @@
-import { useUser } from "@auth0/nextjs-auth0";
-import Link from "next/link";
-import styled from "styled-components";
+import { useUser } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
+import { Button } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 const LoginSection: React.FC = () => {
-  const { user, isLoading } = useUser();
+	const { user, isLoading } = useUser();
 
-  if (isLoading) {
-    return <Container></Container>;
-  }
+	if (isLoading) {
+		return <Container></Container>;
+	}
 
-  if (typeof user === "undefined") {
-    return (
-      <Container>
-        <h1>로그인하고 시작하기</h1>
-        <p>sumit에 처음 오셨나요?</p>
-        <Link href={"/api/auth/login"} passHref>
-          <LoginAnchor>Login</LoginAnchor>
-        </Link>
-      </Container>
-    );
-  }
+	if (typeof user === 'undefined') {
+		return (
+			<Container>
+				<h1>로그인하고 sumit을 시작해보세요!</h1>
+				<Link href={'/api/auth/login'} passHref>
+					<Button animated='fade'>
+						<Button.Content visible>Sumit에 처음 오셨나요?</Button.Content>
+						<Button.Content hidden>Go to Login</Button.Content>
+					</Button>
+				</Link>
+				<Link href={'/search'} passHref>
+					<Button animated='fade'>
+						<Button.Content visible>페이지 구경하기</Button.Content>
+						<Button.Content hidden>Go to My SearchPage</Button.Content>
+					</Button>
+				</Link>
+			</Container>
+		);
+	}
 
-  return (
-    <Container>
-      <h1>
-        환영합니다
-        <br />
-        {user.name}님!
-      </h1>
-      <p>페이지를 한번 만들어볼까요?</p>
-      <Link href={"/dashboard"} passHref>
-        <LoginAnchor>대시보드로 가기</LoginAnchor>
-      </Link>
-    </Container>
-  );
+	return (
+		<Container>
+			<h1>환영합니다, {user.name}님!</h1>
+			<Link href={'/dashboard'} passHref>
+				<Button animated='fade' className='btn'>
+					<Button.Content visible>페이지를 한번 만들어볼까요?</Button.Content>
+					<Button.Content hidden>Go to My Dashboard</Button.Content>
+				</Button>
+			</Link>
+			<Link href={'/search'} passHref>
+				<Button animated='fade'>
+					<Button.Content visible>페이지 구경하기</Button.Content>
+					<Button.Content hidden>Go to My SearchPage</Button.Content>
+				</Button>
+			</Link>
+		</Container>
+	);
 };
 
 export default LoginSection;
 
 const Container = styled.div`
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  padding: 40px 20px;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	padding-top: 50%;
+	background-color: rgba(0, 0, 0, 0.2);
 
-  & > h1 {
-    font-size: 32px;
-    font-weight: 700;
-  }
+	& > h1 {
+		font-size: 44px;
+		font-weight: 600;
+	}
 
-  & > p {
-    font-size: 16px;
-    font-weight: 400;
-  }
-`;
-
-const LoginAnchor = styled.a`
-  display: block;
-  position: absolute;
-  bottom: 100px;
-  box-sizing: border-box;
-  width: 180px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background-color: blue;
-  color: white;
+	& > .btn {
+		border: 1px solid red;
+	}
 `;
